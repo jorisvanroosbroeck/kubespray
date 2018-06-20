87e3b1adf4f67147840f6b3190c0b18467190f0a
Joris Van Roosbroeck <joris.vanroosbroeck@itnetx.be>
2018-06-20 18:17:22 +0200

    create-ansible-hostfile.sh

--- extra_playbooks/k8sswiss/files/create-ansible-hostfile.sh
+++ extra_playbooks/k8sswiss/files/create-ansible-hostfile.sh
@@ -5,14 +5,22 @@
 MASTERCOUNT=2
 ETCDCOUNT=0
 WORKERCOUNT=1
+HOSTFILE="host.ini"
 
 
 # FUNCTIONS
 # =========
 function getVMs {
+    HOSTNAME="master"
+
+    echo "[kube-master]" > $HOSTFILE
+
     for (( i=0;i<$MASTERCOUNT;i++))
     do
-        echo "$i = $MASTERCOUNT"
+        COUNT=$MASTERCOUNT+1
+        HOSTNAME="$HOSTNAME" + "0" + "$COUNT"
+
+        echo "$HOSTNAME"
     done
 }
 
