edb27ba54115aad24da0bd73152a101d4eb94c34
Joris Van Roosbroeck <joris.vanroosbroeck@itnetx.be>
2018-07-09 18:52:21 +0200

    HIAG Cloud 4.0 - Kubespray

--- extra_playbooks/k8sswiss/testing.sh
+++ extra_playbooks/k8sswiss/testing.sh
@@ -21,9 +21,9 @@ ssh-keyscan -H c57b-worker-1 >> .ssh/known_hosts
 ssh-keyscan -H c57b-worker-2 >> .ssh/known_hosts
 
 # Bootstrap OS'en
-#ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-bastion-remote.yml" -vvvvv
-#ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-bastion-local.yml" -vvvvv
-ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-cluster.yml" -vvvvv
+#ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-bastion-remote.yml" -v
+#ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-bastion-local.yml" -v
+ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-cluster.yml" -v
 
 # Install Kubespray
 echo "ansible-playbook --ask-become-pass -b -i '$HOME/kubespray/inventory/k8sswiss/hosts.ini' '$HOME/kubespray/cluster.yml'"
