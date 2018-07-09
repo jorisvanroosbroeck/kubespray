bdc41d75c389dd0a95ca90a8f6e81a84fd782c3b
Joris Van Roosbroeck <joris.vanroosbroeck@itnetx.be>
2018-07-09 18:38:59 +0200

    HIAG Cloud 4.0 - Kubespray

--- extra_playbooks/k8sswiss/testing.sh
+++ extra_playbooks/k8sswiss/testing.sh
@@ -6,6 +6,20 @@ sudo apt-get install python python-pip -y
 # Install Kubespray requirements
 pip install -r "$HOME/kubespray/requirements.txt"
 
+# SSH fingerprints
+ssh-keyscan -H 172.16.1.10 >> .ssh/known_hosts
+ssh-keyscan -H 172.16.1.11 >> .ssh/known_hosts
+ssh-keyscan -H 172.16.1.20 >> .ssh/known_hosts
+ssh-keyscan -H 172.16.1.21 >> .ssh/known_hosts
+ssh-keyscan -H 172.16.1.22 >> .ssh/known_hosts
+ssh-keyscan -H 172.16.1.30 >> .ssh/known_hosts
+ssh-keyscan -H c57b-cm-0 >> .ssh/known_hosts
+ssh-keyscan -H c57b-master-0 >> .ssh/known_hosts
+ssh-keyscan -H c57b-master-1 >> .ssh/known_hosts
+ssh-keyscan -H c57b-worker-0 >> .ssh/known_hosts
+ssh-keyscan -H c57b-worker-1 >> .ssh/known_hosts
+ssh-keyscan -H c57b-worker-2 >> .ssh/known_hosts
+
 # Bootstrap OS'en
 ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-bastion-remote.yml"
 ansible-playbook --ask-become-pass -b -i "$HOME/kubespray/inventory/k8sswiss/hosts.ini" "$HOME/kubespray/extra_playbooks/k8sswiss/bootstrap-k8s-bastion-local.yml"
